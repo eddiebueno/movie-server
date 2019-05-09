dfec6315d76a535c33074d7dade84204fd98a475
Eddie Bueno <eddiebueno93@gmail.com>
2019-05-09 11:45:09 -0700

    changed database reference in config

--- src/config.js
+++ src/config.js
@@ -2,6 +2,6 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DB_URL: process.env.DB_URL || 'postgresql://ebueno@localhost/movie-app',
+  DB_URL: process.env.DATABASE_URL || 'postgresql://ebueno@localhost/movie-app',
   JWT_SECRET: process.env.JWT_SECRET
 };
